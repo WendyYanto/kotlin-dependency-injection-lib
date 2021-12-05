75a548d8887b61f5b3bd0787c764ced1a366e830
wendyyanto <yanto.wendy98@gmail.com>
2021-12-05 21:12:04 +0700

    remove log

--- app/src/main/java/dev/wendyyanto/manual_di_sample/presenter/MainPresenterImpl.kt
+++ app/src/main/java/dev/wendyyanto/manual_di_sample/presenter/MainPresenterImpl.kt
@@ -1,6 +1,5 @@
 package dev.wendyyanto.manual_di_sample.presenter
 
-import android.util.Log
 import dev.wendyyanto.manual_di_sample.annotation.Inject
 import dev.wendyyanto.manual_di_sample.utils.MainUtils
 import dev.wendyyanto.manual_di_sample.utils.StringUtils
@@ -9,10 +8,6 @@ class MainPresenterImpl @Inject constructor(
   private val mainUtils: MainUtils,
   private val stringUtils: StringUtils
 ) : MainPresenter {
-
-  init {
-    Log.v("INJECT", "created MainPresenter")
-  }
 
   override fun getId(): String {
     return mainUtils.formatValue("ID") + " " + stringUtils.test()
